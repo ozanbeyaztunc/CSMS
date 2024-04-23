10f20f0c19837d57c69d7a82ddbdc40f3a20c3fd
Ozan Beyaztunç <ozanbeyaztunc@hotmail.com>
2024-04-23 20:50:41 +0300

    Create placeholder.js

---
+++ placeholder.js
@@ -0,0 +1,13 @@
+// EmptyComponent.js
+
+import React from 'react';
+
+const EmptyComponent = () => {
+  return (
+    <div>
+      {/* Placeholder content */}
+    </div>
+  );
+}
+
+export default EmptyComponent;
